62654658871f33ce4ad5ebb92059d7977fa39c27
Petr Valenta <petr.valenta.92@gmail.com>
2023-09-13 08:48:49 +0200

    Add eventListener to draw cards button

--- index.js
+++ index.js
@@ -8,4 +8,14 @@ function getDeck() {
         });
 }
 
+function drawCards() {
+    fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
+        .then(respo => respo.json())
+        .then(data => {
+            console.log(data.cards);
+        });
+}
+
 document.getElementById('new-deck').addEventListener('click', getDeck);
+
+document.getElementById('draw-cards').addEventListener('click', drawCards);
