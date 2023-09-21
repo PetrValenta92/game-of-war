0004ff64827d392a4cbd5385e9e6e166f52c57e5
Petr Valenta <petr.valenta.92@gmail.com>
2023-09-21 21:53:29 +0200

    Add comments

--- index.js
+++ index.js
@@ -19,3 +19,28 @@ function drawCards() {
 document.getElementById('new-deck').addEventListener('click', getDeck);
 
 document.getElementById('draw-cards').addEventListener('click', drawCards);
+
+// -------------- CALLBACK FUNCTIONS --------------
+
+// Functions are first-class objects in JavaScript
+
+// Function can be saved as a variable
+
+// "Function declaration" 
+// function myFunction() {return ???};
+
+// "Function expression"
+// let myFunction = function() {return ???};
+
+// myFunction x myFunction() 
+// myFunction == called when it's time to call it (callback)
+// myFunction() => called instantly
+
+// ---------------------- PROMISES -----------------
+
+// Special object in JavaScript
+
+// Has three states:
+// Pending - The promise has yet to be completed
+// Fulfilled - The promise was completed as promised
+// Rejected - The promise was not completed as promised
