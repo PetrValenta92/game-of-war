53ec6f04eac47a39dd584eee9b31f6c123c5d105
Petr Valenta <petr.valenta.92@gmail.com>
2023-10-04 23:09:25 +0200

    Add functions

--- index.js
+++ index.js
@@ -1,16 +1,8 @@
 let deckId;
 const drawBtn = document.getElementById('draw-cards');
 const cardsCount = document.getElementById('remaining-cards');
-
-function enableBtn() {
-    drawBtn.classList.remove('disabled');
-    drawBtn.disabled = false;
-}
-
-function disableBtn() {
-    drawBtn.classList.add('disabled');
-    drawBtn.disabled = true;
-}
+let compScore;
+let userScore;
 
 function getDeck() {
     fetch('https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/')
@@ -18,16 +10,7 @@ function getDeck() {
         .then(data => {
             deckId = data.deck_id;
             cardsCount.textContent = `Remaining cards: ${data.remaining}`;
-            enableBtn();
         });
-}
-
-function renderImages(cards) {
-    for (let i = 0; i < cards.length; i++) {
-        document.getElementById("cards-images").children[i].innerHTML = `
-            <img class="card" src="${cards[i].image}" alt="Image of card">
-        `
-    }
 }
 
 function getResult(card1, card2) {
@@ -39,29 +22,75 @@ function getResult(card1, card2) {
     cardValues.indexOf(card2.value)
     
     if(cardValues.indexOf(card1.value) > cardValues.indexOf(card2.value)) {
-        return "Computer wins!";
+        compScore++;
+        return "Computer wins the battle!";
     } else if(cardValues.indexOf(card1.value) < cardValues.indexOf(card2.value)) {
-        return "You win!"
+        userScore++;
+        return "You win the battle!"
     } else {
-        return "War!"
+        return "Keep fighting!"
     }
+}
+
+function renderScore() {
+    document.getElementById('computer-score').textContent = `Computer's Score: ${compScore}`;
+    document.getElementById('user-score').textContent = `User's Score: ${userScore}`;
+}
+
+function restartScore() {
+    compScore = 0;
+    userScore = 0;
+    renderScore();
+}
+
+function renderCards(cards) {
+    for (let i = 0; i < cards.length; i++) {
+        document.getElementById("cards-images").children[i].innerHTML = `
+            <img class="card" src="${cards[i].image}" alt="Image of card">
+        `
+    }
+}
+
+function getWinner() {
+    if (compScore > userScore) {
+        return 'Computer has won!💀'
+    } else if (compScore < userScore) {
+        return 'You have won!🚀'
+    } else {
+        return "It's a tie!🙃"
+    }
+}
+
+function enableBtn() {
+    drawBtn.classList.remove('disabled');
+    drawBtn.disabled = false;
+}
+
+function disableBtn() {
+    drawBtn.classList.add('disabled');
+    drawBtn.disabled = true;
 }
 
 function drawCards() {
     fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
         .then(respo => respo.json())
         .then(data => {
-            renderImages(data.cards);
+            renderCards(data.cards);
             document.getElementById('result').textContent = getResult(data.cards[0], data.cards[1]);
             cardsCount.textContent = `Remaining cards: ${data.remaining}`;
             if (data.remaining <= 0) {  
                 disableBtn();
+                document.getElementById('result').textContent = getWinner();
             }
-            
+            renderScore();
         });
 }
 
-document.getElementById('new-deck').addEventListener('click', getDeck);
+document.getElementById('new-deck').addEventListener('click', () => {
+    getDeck();
+    restartScore();
+    enableBtn();
+});
 
 drawBtn.addEventListener('click', drawCards);
 
